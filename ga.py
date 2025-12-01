b235bf01173a89c4987a7407bc85eea000daef1b
mk306207 <kolbermateusz@gmail.com>
2025-12-01 15:44:20 +0100

    population for Bird

--- ga.py
+++ ga.py
@@ -21,6 +21,19 @@ class genetic_algorithm:
                 print(self.population)
             #Populaiton for maze genom = [1,2,1,3,4] 1= left, 2 = right, 3 = go up, 4 = go down
         elif self.game == "Bird":
-            pass #Population for bird genom = [y_weight,x_weight,velocity_weight,bias]
+            for chromosome in range(0,self.population_size):
+                genom = []
+                for i in range(0,200):
+                    w_y   = random.uniform(-2.0, 2.0)
+                    w_x   = random.uniform(-1.0, 1.0)
+                    w_v   = random.uniform(-2.0, 2.0)
+                    bias  = random.uniform(-1.0, 1.0)
+                    genom.append(w_y)
+                    genom.append(w_x)
+                    genom.append(w_v)
+                    genom.append(bias)
+                self.population.append(genom)
+                print(self.population)
+            #Population for bird genom = [y_weight,x_weight,velocity_weight,bias]
 
-genetic_algorithm("Maze").create_population()
+genetic_algorithm("Bird").create_population()
